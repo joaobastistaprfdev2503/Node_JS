206eb9295ba851bb53d250b2beeab3b2fade9685
Jaumzeirakkkj <joaobatistaprf7@gmail.com>
2026-02-10 23:56:55 -0300

    atualizaçao dos estudos

--- aula3.js
+++ aula3.js
@@ -4,7 +4,25 @@
 // JSON.parse(conteudo) =>  passa o arquivo de JSON para java script 
 
 
-/* const fs = require("fs");
+// Fiz o requerimento para o node ler os arquivos do meu sistema
+/* const fs = require("fs")
+
+//Criei um array de produtos, li meu array com stringify e transformei em string                        
+const produtos = ["maça", "pera", "melancia", "batata"]
+const json = JSON.stringify(produtos, null, 2)
+//
+fs.writeFileSync("produtos.json", json)
+
+
+const conteudo = fs.readFileSync("produtos.json", "utf-8")
+const produto = JSON.parse(conteudo)
+
+console.log(produto[0]) */
+
+
+
+
+const fs = require("fs");
 
 const usuarios = [
   { id: 1, nome: "João", idade: 25 },
@@ -17,23 +35,14 @@ const json = JSON.stringify(usuarios, null, 2);
 // cria o arquivo
 fs.writeFileSync("usuarios.json", json);
 
-console.log("Arquivo usuarios.json criado!"); */
+
+const conteudo = fs.readFileSync("usuarios.json", "utf-8") // li o json de usuarios que criei
+
+const usuariosFiltrados = JSON.parse(conteudo)//passei o JSON para objeto e filtrei user.idade >= 25
+  .filter(usuario => usuario.idade > 25)
+
+console.log(usuariosFiltrados)
 
 
-// Fiz o requerimento para o node ler os arquivos do meu sistema
-const fs = require("fs")
-
-//Criei um array de produtos, li meu array com stringify e transformei em string                        
-const produtos = ["maça", "pera", "melancia", "batata"]
-const json = JSON.stringify(produtos, null, 2)
-//
-fs.writeFileSync("produtos.json", json)
-
-
-const conteudo = fs.readFileSync("produtos.json", "utf-8")
-const produto = JSON.parse(conteudo)
-
-console.log(produto[0])
-
-
-
+const json2 = JSON.stringify(usuariosFiltrados, null, 2)
+fs.writeFileSync("usuariosFiltrados.json", json2)
